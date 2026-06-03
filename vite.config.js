011753cfae4c79f0ea2f618dccc75809ef1ffe3a
Girish Kumar Tripurana <tgirishkumar@yahoo.co.in>
2026-06-03 12:58:54 +0100

    Fix build/test warnings: upgrade Vite 8, exclude Playwright tests from Vitest

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    include: ['src/**/*.test.js'],
+    exclude: ['tests/**', 'node_modules/**'],
+  },
 });
